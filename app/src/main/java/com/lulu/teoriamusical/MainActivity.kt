b4c5e3111623e198d5d283428dfc891eeb3c40b0
luclucas <luclucas.08@gmail.com>
2022-08-08 23:48:27 -0300

    Começo do trabalho

---
+++ app/src/main/java/com/lulu/teoriamusical/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.lulu.teoriamusical
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adaptador: RecyclerView.Adapter<RecyclerAdapterInicial.ViewHolder>? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val recyclerView= findViewById<RecyclerView>(R.id.recyclerViewInicial)
+
+        layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        adaptador = RecyclerAdapterInicial()
+        recyclerView.adapter = adaptador
+    }
+}
---
+++ app/src/main/java/com/lulu/teoriamusical/RecyclerAdapterInicial.kt
@@ -0,0 +1,43 @@
+package com.lulu.teoriamusical
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapterInicial: RecyclerView.Adapter<RecyclerAdapterInicial.ViewHolder>() {
+
+    private val titulos = arrayOf("Módulo I", "Módulo II", "Módulo III", "Módulo IV")
+    private val descricoes = arrayOf("Conteudo I", "Conteudo II", "Conteudo III", "Conteudo IV")
+    private val imagens = arrayOf(R.drawable.img_semibreve, R.drawable.img_minima,
+    R.drawable.img_seminima, R.drawable.img_colcheia)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType:Int):RecyclerAdapterInicial.ViewHolder{
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.card_tela_inicial, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return titulos.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapterInicial.ViewHolder, position: Int) {
+        holder.tituloItem.text = titulos[position]
+        holder.descricaoItem.text = descricoes[position]
+        holder.imagemItem.setImageResource(imagens[position])
+    }
+
+    inner class ViewHolder(itenView: View): RecyclerView.ViewHolder(itenView){
+        var imagemItem: ImageView
+        var tituloItem: TextView
+        var descricaoItem: TextView
+
+        init {
+            imagemItem = itemView.findViewById(R.id.imagem_item)
+            tituloItem = itemView.findViewById(R.id.titulo_item)
+            descricaoItem = itemView.findViewById(R.id.descricao_item)
+        }
+    }
+}
